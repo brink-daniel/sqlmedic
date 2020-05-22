6f3ec12c0f3e6c0dfa887282d1cacb52b2f50308
Daniel Brink <brink.daniel@gmail.com>
2020-05-22 22:38:32 +1000

    Create basic content pages

--- app.py
+++ app.py
@@ -5,5 +5,16 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-	return render_template("base.html")
+	return render_template("home.html")
 
+@app.route("/about/")
+def about():
+	return render_template("about.html")
+
+@app.route("/contact/")
+def contact():
+	return render_template("contact.html")
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
