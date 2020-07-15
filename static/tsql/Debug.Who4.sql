abdabd616654170a093aaeb4ef96db1951b7bb5d
Daniel Brink <brink.daniel@gmail.com>
2020-07-15 20:22:31 +1000

    Refinements to script logic

--- static/tsql/Debug.Who4.sql
+++ static/tsql/Debug.Who4.sql
@@ -21,24 +21,25 @@
 set transaction isolation level read uncommitted
 
 
-
 select
 	r.session_id as spid
 	,r.blocking_session_id as blocked_by
 	,r.status
-	,isnull(object_name(t.objectid, t.dbid), i.event_info) as object_name
-	,db_name(r.database_id) as [db]
-	,convert(decimal(26,2), r.total_elapsed_time / 1000.0) as total_elapsed_time
-	,r.reads
-	,r.writes
-	,r.logical_reads
-	,r.row_count
-	,r.granted_query_memory
-	,r.open_transaction_count	
-	,isnull(j.name, s.program_name) as program_name
-	,s.login_name
+	,isnull(cast(object_name(t.objectid, t.dbid) as varchar(1000)), i.event_info) as object_name	
+	,db_name(r.database_id) as [db]	
+	,convert(decimal(26,2), r.cpu_time / 1000.0) as cpu_time
+	,convert(decimal(26,2), r.total_elapsed_time / 1000.0) as total_time
+	,(r.reads * 8) / 1000 as reads_mb
+	,(r.writes * 8) / 1000 as writes_mb
+	,cast((r.logical_reads * 8) / 1000000.0 as decimal(26,2)) as logical_reads_gb
+	,r.row_count as [rows]
+	,(r.granted_query_memory * 8) /1000 as memory_mb
+	,r.open_transaction_count as open_tran
+	,isnull(j.name, s.program_name) as program	
+	,isnull(nullif(s.login_name, ''), s.original_login_name) as login
 	,s.host_name
 	,try_cast(p.query_plan as xml) as query_plan
+	
 
 from
 	sys.dm_exec_requests as r
@@ -57,7 +58,9 @@ from
 	on j.job_id = case when s.program_name like 'SQLAgent - TSQL JobStep (Job % : Step %)' then cast(Convert(binary(16), substring(s.program_name, 30, 34), 1) as uniqueidentifier) else null end
 				
 		
-where r.session_id <> @@spid
+where 
+	r.session_id <> @@spid
+	and r.cpu_time > 0
 
 order by
 	r.total_elapsed_time desc
